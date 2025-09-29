6d148cfa23f8873a541ffdf71e88baa8e833dba8
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-29 22:30:38 +0200

    uso del repositorio en los controladores

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,6 +1,7 @@
 import {Request,Response} from 'express';
 import { prisma } from '../../data/postgres';
 import { CreateTodoDto, UpdateTodoDto } from '../../domain/dtos';
+import { TodoRepository } from '../../domain';
 /*
 const todos=[
                 {id:1, text:'Buy milk', completedAt:new Date()},
@@ -10,44 +11,34 @@ const todos=[
 */
 export class TodosController{
 
+    constructor(
+        private readonly todoRepository:TodoRepository
+    ){}
+
     public getTodos=async(req:Request, res:Response) => {
-        const todos=await prisma.todo.findMany();
-        return res.json(todos)
+        const todos=await this.todoRepository.getAll();
+        return res.json(todos);
     }
     
     public getTodoById=async(req:Request,res:Response)=>{
         const id=+req.params.id;
         if(isNaN(id)) return res.status(400).json({error:'ID argument is not a number'});
 
-        const todo=await prisma.todo.findFirst({
-            where:{id}
-        });
-        //const todo=todos.find(todo=>todo.id===id);
-        (todo)
-            ? res.json(todo)
-            : res.status(404).json({error:`TODO with id ${id} not found`});
+        try {
+           const todo=await this.todoRepository.findById(id);
+           res.json(todo);
+        } catch (error) {
+            res.status(400).json({error})
+        }
+
     }
 
     public createTodo=async(req:Request, res:Response) => {
         
-        //const {text}=req.body;
         const [error,createTodoDto]=CreateTodoDto.create(req.body || {});
         if(error) return res.status(400).json({error});
 
-        //if(!text) return res.status(400).json({error: 'Text property is required'});
-
-        const todo=await prisma.todo.create({
-            data: createTodoDto!
-        });
-        /*
-        const newTodo={
-            id:todos.length+1,
-            text: text,
-            completedAt:null
-        }
-        
-        todos.push(newTodo);
-        */
+        const todo=await this.todoRepository.create(createTodoDto!);
         res.json(todo);
         
     }
@@ -57,48 +48,15 @@ export class TodosController{
         const id=+req.params.id;
         const [error, updateTodoDto]=UpdateTodoDto.create({...req.body,id});
         if(error) return res.status(400).json({error});
-        //if(isNaN(id)) return res.status(400).json({error:'ID argument is not a number'});
 
-        const todo=await prisma.todo.findFirst({
-            where:{id}
-        });
-        //const todo=todos.find(todo=>todo.id===id);
-        if(!todo) return res.status(404).json({error:`Todo with id ${id} not found`});
-
-        //const {text,completedAt}=req.body;
-        //if(!text) return res.status(400).json({error: 'Text property is required'});
-        /*
-        todo.text=text || todo.text;
-        (completedAt==='null')
-            ? todo.completedAt=null
-            : todo.completedAt=new Date(completedAt || todo.completedAt);
-        */
-        const updatedTodo=await prisma.todo.update({
-            where:{id},
-            data:updateTodoDto!.values
-        });
-        res.json(updatedTodo);
-        
+        const updatedTodo=await this.todoRepository.updateById(updateTodoDto!);
+        return res.json(updatedTodo);
     }
 
     public deleteTodo=async(req:Request, res:Response) => {
         const id=+req.params.id;
-        if(isNaN(id)) return res.status(400).json({error:'ID argument is not a number'});
-
-        const todo=await prisma.todo.findFirst({
-            where:{id}
-        });
-        //const todo=todos.find(todo=>todo.id===id);
-        if(!todo) return res.status(404).json({error:`Todo with id ${id} not found`});
-
-        //todos.splice(todos.indexOf(todo),1);
-        const deleted=await prisma.todo.delete({
-            where:{id}
-        });
-
-        (deleted)
-            ? res.json(deleted)
-            : res.status(400).json({error:`Todo with id ${id} not exists`});
+        const deletedTodo=await this.todoRepository.deleteById(id);
+        res.json(deletedTodo);
     }
 
 }
--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { TodosController } from "./controller";
+import { TodoDatasourceImpl } from "../../infrastructure/datasource/todo.datasource.impl";
+import { TodoRepositoryImpl } from "../../infrastructure/repositories/todo.repository.impl";
 
 export class TodoRoutes{
 
     static get routes():Router{
 
         const router=Router();
-        const todosController=new TodosController();
+
+        const datasource=new TodoDatasourceImpl();
+        const todoRepository=new TodoRepositoryImpl(datasource);
+        const todosController=new TodosController(todoRepository);
 
         router.get('/', todosController.getTodos);
         router.get('/:id', todosController.getTodoById);
