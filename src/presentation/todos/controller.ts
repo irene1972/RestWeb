b3ec5bb2b7a398894d177540cdc6b2d6b3354535
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-29 09:05:07 +0200

    crear registro

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -23,8 +23,16 @@ export class TodosController{
     }
 
     public createTodo=(req:Request, res:Response) => {
-        const body=req.body;
-        res.json(body);
+        const {text}=req.body;
+        if(!text) return res.status(400).json({error: 'Text property is required'});
+
+        const newTodo={
+            id:todos.length+1,
+            text: text,
+            createdAt:null
+        }
+        todos.push(newTodo);
+        res.json(newTodo);
     }
 
 }
