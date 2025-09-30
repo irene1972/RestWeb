936fb765f09371c704f2facc1f8c40693a92143a
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-30 11:23:42 +0200

    prueba GET ALL y GET BY ID

---
+++ tests/presentation/todos/routes.test.ts
@@ -0,0 +1,71 @@
+import request from "supertest";
+import {testServer} from '../../test-server';
+import { prisma } from "../../../src/data/postgres";
+
+describe('routes.ts',()=>{
+
+    beforeAll(async()=>{
+        await testServer.start();
+    });
+
+    afterAll(()=>{
+        testServer.close();
+    });
+
+    beforeEach(async()=>{
+        await prisma.todo.deleteMany();
+    });
+
+    const todo1={text:'Hola Mundo 1'};
+    const todo2={text:'Hola Mundo 2'};
+
+    test('should return TODOs api/todos',async()=>{
+        
+        await prisma.todo.createMany({
+            data: [todo1,todo2]
+        })
+
+        const {body}=await request(testServer.app)
+        .get('/api/todos')
+        .expect(200);
+
+        console.log(body);
+        expect(body).toBeInstanceOf(Array);
+        expect(body.length).toBe(2);
+        expect(body[0].text).toBe(todo1.text);
+        expect(body[1].text).toBe(todo2.text);
+        expect(body[0].completedAt).toBeNull();
+
+    });
+
+    test('should return a TODO api/todos/:id',async()=>{
+
+        const todo=await prisma.todo.create({
+            data: todo1
+        })
+        
+        const {body}=await request(testServer.app)
+        .get(`/api/todos/${todo.id}`)
+        .expect(200);
+
+        //console.log('irene:',body);
+        expect(body).toEqual({
+            id: todo.id, 
+            text: todo.text, 
+            completedAt: todo.completedAt
+        });
+    });
+
+    test('should return a 404 NotFound api/todos/:id',async()=>{
+
+        const todoId=1;
+        const {body}=await request(testServer.app)
+        .get(`/api/todos/${todoId}`)
+        .expect(400);
+
+        //console.log('irene:',body);
+        expect(body).toEqual({error: `Todo with id ${todoId} not found`});
+
+    });
+
+});
